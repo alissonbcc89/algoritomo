4b6aeff20f2a7d188afc8b9305321e9b91b41f3c
Alisson Cavalcanti <ajsc@ibm.com>
2022-08-12 16:14:13 -0300

    Andamento da terceira questao

--- src/SubArray.java
+++ src/SubArray.java
@@ -16,9 +16,7 @@ public class SubArray {
 
             if (soma < 0) {
                 soma = 0;
-            }
-            else
-            {
+            } else {
                 somas.add(soma);
             }
 
@@ -26,5 +24,34 @@ public class SubArray {
         int maxValor = Collections.max(somas);
         return maxValor;
 
+    }
+
+    public int minDiffer(int[] vetor1, int[] vetor2)
+    {
+        int diferenca = 0;
+        int n = vetor1.length;
+        int m = vetor2.length;
+        ArrayList<Integer> array = new ArrayList<>();
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                diferenca =  Math.abs((vetor1[i] - vetor2[j]));
+                array.add(vetor1[i]);
+                array.add(vetor2[j]);
+                array.add(diferenca);
+            }
+        }
+
+        int minValor = Collections.min(array);
+        int[] v ;
+
+        for(int i = 0; i < array.size(); i++){
+            if (minValor == array.get(i)){
+
+            }
+        }
+
+        return diferenca;
     }
 }
