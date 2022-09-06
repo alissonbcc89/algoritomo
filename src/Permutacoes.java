45a863bcbe2b8dd586485f9915753aac0fd955a8
Alisson Cavalcanti <ajsc@ibm.com>
2022-09-05 23:39:46 -0300

    eu tou tentando muiitoooo

--- src/Permutacoes.java
+++ src/Permutacoes.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Permutacoes {
 
     //numero da permutacao atual
@@ -44,6 +46,7 @@ public class Permutacoes {
 
                     p[n] = vet[i];
                     permuta(vet,n+1);
+
                 }
 
             } //--for
@@ -51,14 +54,49 @@ public class Permutacoes {
         } //--if/else
 
     } //--permuta
+    public static ArrayList<Integer> concatena(int[] array){
+        ArrayList<Integer> vetor = new ArrayList<Integer>();
+        String aux = "";
+            for(int i =0; i < array.length;i++){
+                aux+= array[i];
+            }
+            int i = Integer.parseInt(aux);
+            vetor.add(i);
+            return vetor;
+
+    }
+    public static int maiorAbs(ArrayList<Integer> arr){
+
+        // Create maxValue variable and initialize with 0
+        int maxValue = 0;
+
+        for(int i = 0; i < arr.size(); i++){
+
+        }
+
+        // Check maximum element using for loop
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > maxValue)
+                maxValue = arr.get(i);
+        }
+        return maxValue;
+    }
+
+
 
 
     /** imprime a permutacao corrente */
     private static void imprime() {
 
-        System.out.println();
-        System.out.print("(" + cont + ") : ");
-        for (int i=0; i < p.length; i++) System.out.print(p[i] + " ");
+       // System.out.println();
+        //System.out.print("(" + cont + ") : ");
+        //for (int i=0; i < p.length; i++) {System.out.print(p[i] );}
+
+        System.out.println(maiorAbs(concatena(p)));
+       // System.out.println(p.length);
+
+
+       // System.out.println(maiorAbs(p));
 
     } //--imprime
 
