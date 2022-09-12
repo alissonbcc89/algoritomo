1ddcb30824e26fda4bbc483bf8f1e9b417136ab1
Alisson Cavalcanti <ajsc@ibm.com>
2022-09-12 09:49:09 -0300

    finalizado

--- src/ContaStringInt.java
+++ src/ContaStringInt.java
@@ -1,51 +1,56 @@
 import java.util.ArrayList;
 
 public class ContaStringInt {
-    private int contador = 0;
-    public static void funcao(String myString){
-            char[] myChars = myString.toCharArray();
-            String aux;
-            ArrayList<String> array;
-            array = new ArrayList<>();
+    private static int contador = 1;
+    private static ArrayList<String> array = new ArrayList<>();
 
-            int contador = 0;
-
-            for(int i=0; i < myChars.length; i++){
-                aux = String.valueOf(myChars[i]);
-                for(int j = 1; j < myChars.length; j++) {
-                    if (aux == String.valueOf(myChars[j])) {
-                        contador++;
-                        aux = String.valueOf(myChars[i+1]);
-                        break;
-                    }
-                    if (aux != String.valueOf(myChars[j])) {
-                        array.add(aux);
-                        if (contador > 1) {
-                            array.add(String.valueOf(contador));
-                        }
-                        contador = 0;
-
-                    }
-                }
+    public static void funcao(String myString) {
+        char[] myChars = myString.toCharArray();
+        String aux;
 
 
 
-            }
+
+        for (int i = 0; i < myChars.length; i++) {
+
+                funcao(myString,i);
+        }
+
+        for(int a = 0; a < array.size(); a++){
+            System.out.print(array.get(a));
+        }
 
 
-            for (int i = 0; i < array.size(); i++){
-                System.out.println(array.get(i));
-            }
 
 
     }
 
+    public static void funcao(String palavra, int i){
+        char[] myChars = palavra.toCharArray();
+
+        int aux = i;
+
+        while (aux == i){
+            if(i+1 < myChars.length && myChars[i] == myChars[i+1]){
+                contador++;
+
+            }
+            else {
+                array.add(String.valueOf(myChars[i]));
+                array.add(String.valueOf(contador));
+                contador = 1;
+            }
+            aux++;
+        }
 
 
 
-    public static void main(String[] args) {
 
-      //  bbbaaaadexxxxxx
+    }
+
+    public static void main (String[]args){
+
+        //  bbbaaaadexxxxxx
         //
         //        abc
 
@@ -53,8 +58,7 @@ public class ContaStringInt {
         //Permutacoes.permuta(array);
         ContaStringInt.funcao(a);
     }
-    }
-
+}
 
 
 
